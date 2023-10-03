1ce2ff78b5857ce387a1c85d890822d562363445
Jose Yolanda Purba <josprb@gmail.com>
2023-10-03 10:14:45 +0700

    add test helpers

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,38 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox.joseyp.dev/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
+	app := newTestApplication(t)
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	assert.Equal[int](t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bytes.TrimSpace(body)
-
-	assert.Equal[string](t, string(body), "OK")
+	assert.Equal[int](t, code, http.StatusOK)
+	assert.Equal[string](t, body, "OK")
 }
